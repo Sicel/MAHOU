4b39bfe7e02ecac00164ecfaa588cbce751965a0
Cristian <cristiandc10@live.com>
2019-12-31 14:29:04 -0500

    Init

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private GameObject camera;
+    [SerializeField] private float normalSpeed = 5.0f;
+    [SerializeField, Range(1.5f, 2.5f)] private float sprintSpeedMultiplier = 1.5f;
+
+    private Vector2 direction = Vector2.zero;
+    private Vector3 movement = Vector2.zero;
+
+    private float sprintSpeed { get { return normalSpeed * sprintSpeedMultiplier; } }
+
+    private Vector3 cameraForward { 
+        get 
+        {
+            Vector3 forward = new Vector3(camera.transform.forward.x, 0, camera.transform.forward.z);
+            return forward.normalized; 
+        } 
+    }
+
+    private Vector3 cameraRight { 
+        get 
+        {
+            Vector3 right = new Vector3(camera.transform.right.x, 0, camera.transform.right.z);
+            return right.normalized; 
+        } 
+    
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    public void Walk(InputAction.CallbackContext context)
+    {
+        direction = context.ReadValue<Vector2>();
+        movement = Vector3.zero;
+        //movement = new Vector3(direction.x, 0, direction.y) * normalSpeed;
+
+        if (direction.x > 0)
+            movement += cameraRight * normalSpeed;
+        else if (direction.x < 0)
+            movement += -cameraRight * normalSpeed;
+
+        if (direction.y > 0)
+            movement += cameraForward * normalSpeed;
+        else if (direction.y < 0)
+            movement += -cameraForward * normalSpeed;
+    }
+
+    public void Sprint(InputAction.CallbackContext context)
+    {
+        direction = context.ReadValue<Vector2>();
+        movement = new Vector3(direction.x, 0, direction.y) * sprintSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        transform.position += movement * Time.fixedDeltaTime;
+    }
+}
---
+++ Assets/Scripts/ThirdPersonCamera.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ThirdPersonCamera : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+    [SerializeField] private Vector3 offset;
+    [SerializeField] private float sensitivity = 5.0f;
+    [SerializeField] private float height = 5.0f;
+    [SerializeField] private float distanceFromPlayer = 10.0f;
+
+    private float rotateAmount;
+    private Vector3 PlayerPos { get { return player.transform.position; } }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = new Vector3(PlayerPos.x, PlayerPos.y + height, PlayerPos.z + distanceFromPlayer);
+        transform.LookAt(PlayerPos);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //offset = new Vector3(PlayerPos.x, PlayerPos.y + height, PlayerPos.z + distanceFromPlayer);
+    }
+
+    public void RotateCamera(InputAction.CallbackContext context)
+    {
+        Debug.Log(context.action);
+        rotateAmount = context.ReadValue<Vector2>().x;
+    }
+
+    private void FixedUpdate()
+    {
+
+    }
+
+    private void LateUpdate()
+    {
+        offset = Quaternion.AngleAxis(rotateAmount * sensitivity, Vector3.up) * offset;
+        transform.position = PlayerPos + offset;
+        transform.LookAt(PlayerPos);
+    }
+}
